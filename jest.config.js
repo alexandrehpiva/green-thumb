cb1af25a12a115d200c99b19fd3fd0632f6c8562
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-21 04:27:31 -0300

    Config jest import images

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,10 @@ module.exports = {
   },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
-    '<rootDir>/src/jest-setup.ts'
+    '<rootDir>/src/jest-setup.ts',
   ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js",
+  },
 };
