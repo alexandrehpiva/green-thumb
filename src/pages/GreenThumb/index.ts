72d0b07f291a459fa0b01f65cf236b0d1595cbb9
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-21 09:33:30 -0300

    Import images on GreenThumb

--- src/pages/GreenThumb/index.ts
+++ src/pages/GreenThumb/index.ts
@@ -1,10 +1,9 @@
 import Component from '../../lib/Component';
 
-class GreenThumb extends Component {
-  constructor() {
-    super();
-  }
+import logoWhite from '../../assets/images/icons/logo-white.svg';
+import arrowDown from '../../assets/images/icons/arrow-down-mod.svg';
 
+class GreenThumb extends Component {
   render() {
     this.element.innerHTML = `
       <header data-testid="main-header" class="main-header">
@@ -12,17 +11,18 @@ class GreenThumb extends Component {
           <div class="nav-wrapper">
             <div class="nav-left">
               <div class="logo">
-                <img src="../src/assets/images/icons/logo-white.svg" alt="Greenthumb" />
+                <img src="${logoWhite}" alt="Greenthumb" />
               </div>
               <h1 class="text">Find your next green friend</h1>
               <div>
                 <img
-                  src="../src/assets/images/icons/arrow-down-mod.svg"
+                  src="${arrowDown}"
                   alt="Greenthumb"
                 />
               </div>
             </div>
-            <div class="nav-right">
+            <div class="nav-right"
+            { logoWhite: 'test-file-stub', arrowDown: 'test-fil>
               <picture class="nav-right-image"></picture>
             </div>
           </div>
