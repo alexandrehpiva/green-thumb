1818c38531c7a01fae2e35132ff6f7d1a69f7510
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-27 13:10:42 -0300

    Add findByTestIds testing utility

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -1,8 +1,16 @@
-import { queryByAttribute } from '@testing-library/dom';
+import { queryByAttribute, screen } from '@testing-library/dom';
 
 const queryById = queryByAttribute.bind(null, 'id');
 
 const cleanup = () => (document.body.innerHTML = '');
 
+const findByTestIds = <T extends HTMLElement[] = HTMLElement[]>(
+  ...testIds: string[]
+) => {
+  return Promise.all(
+    testIds.map(testId => screen.findByTestId(testId))
+  ) as Promise<T>;
+};
+
 // Additional methods
-export { queryById, cleanup };
+export { queryById, cleanup, findByTestIds };
