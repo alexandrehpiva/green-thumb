a6df1b02c6badbf353dd221913bca6bb4493a307
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-28 22:16:13 -0300

    Prepare EventListener tests

---
+++ src/lib/EventListener.test.ts
@@ -0,0 +1,13 @@
+import EventListener from './EventListener';
+
+describe('Unit(EventListener) - subscribe method', () => {
+  it('should...', () => {
+    // TODO
+  });
+});
+
+describe('Unit(EventListener) - publish method', () => {
+  it('should...', () => {
+    // TODO
+  });
+});
