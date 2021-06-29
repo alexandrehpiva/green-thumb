1c92f100b872e831497f5d1ac7b8c40470d0c1c6
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-28 22:17:08 -0300

    Prepare plantsService tests

---
+++ src/services/plantsService.test.ts
@@ -0,0 +1,9 @@
+describe('Unit(plantsService)', () => {
+  it('should send the api response correctly if no server error', () => {
+    // TODO
+  });
+
+  it('should return a ApiError response when catch some error', () => {
+    // TODO
+  });
+});
