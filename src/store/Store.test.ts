0d241a0c6c8d6d00875915b92460db616460cebe
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-29 14:52:13 -0300

    Add Store tests

--- src/store/Store.test.ts
+++ src/store/Store.test.ts
@@ -1,9 +1,47 @@
+import Store from './Store';
+
+type MockState = {
+  count: number;
+};
+
 describe('Unit(Store)', () => {
+  const initialMockState: MockState = { count: 0 };
+  const mockMutations = {
+    mock: {
+      addToCount(state: MockState, payload: number) {
+        return {
+          count: state.count + payload,
+        };
+      },
+    },
+  };
+
   it('should publish a change event when detect a change in state', () => {
-    // TODO
+    const mockFn = jest.fn();
+
+    const store = new Store({
+      state: {},
+      mutations: {},
+    });
+
+    store.events.subscribe('change', mockFn);
+
+    store.events.publish('change');
+
+    expect(mockFn).toBeCalledTimes(1);
   });
 
   it('should change the state with the payload when dispatch an mutation', () => {
-    // TODO
+    const state = { mock: initialMockState };
+    const store = new Store({
+      state,
+      mutations: mockMutations,
+    });
+
+    expect(state.mock).toEqual({ count: 0 });
+
+    store.dispatch('mock', 'addToCount', 2);
+
+    expect(state.mock).toEqual({ count: 2 });
   });
 });
