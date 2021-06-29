718deb3ef1a80a8ce4adf73621a0619911caf985
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-28 22:17:32 -0300

    Prepare Store tests

---
+++ src/store/Store.test.ts
@@ -0,0 +1,9 @@
+describe('Unit(Store)', () => {
+  it('should publish a change event when detect a change in state', () => {
+    // TODO
+  });
+
+  it('should change the state with the payload when dispatch an mutation', () => {
+    // TODO
+  });
+});
