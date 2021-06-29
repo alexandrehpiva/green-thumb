471714ca50467d4605f42896ee55c6a4fbfb1696
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-28 22:18:44 -0300

    Write tests for isKeyOfCombinedStates

---
+++ src/utils/typeValidators/isKeyOfCombinedStates.test.ts
@@ -0,0 +1,20 @@
+import { CombinedStates } from '../../store/state';
+import isKeyOfCombinedStates from './isKeyOfCombinedStates';
+
+describe('Unit(isKeyOfCombinedStates)', () => {
+  const mockCombinedStatesObj: CombinedStates = {
+    main: {
+      filters: {
+        sunlight: 'low',
+      },
+    },
+  };
+
+  it('should return true the propName is a key of the obj of type CombinedStates', () => {
+    expect(isKeyOfCombinedStates('main', mockCombinedStatesObj)).toBe(true);
+  });
+
+  it('should return false the propName is a key of the obj of type CombinedStates', () => {
+    expect(isKeyOfCombinedStates('navbar', mockCombinedStatesObj)).toBe(false);
+  });
+});
