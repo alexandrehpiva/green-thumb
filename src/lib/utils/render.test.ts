de899bcc7a92d8a73cc384c236928a2a59dba1ce
Alexandre Piva <alexandrehpiva@gmail.com>
2021-06-29 16:23:05 -0300

    Add tests for render function

--- src/lib/utils/render.test.ts
+++ src/lib/utils/render.test.ts
@@ -1,5 +1,40 @@
+import { within } from '@testing-library/dom';
+import Component from '../Component';
+import render from './render';
+
 describe('Unit(render)', () => {
-  it('should...', () => {
-    // TODO
+  class TestComponent extends Component {
+    render() {
+      this.element.innerHTML = `
+        <div data-testid="test-element"></div>
+      `;
+    }
+  }
+
+  class EmptyComponent extends Component {}
+
+  it('should call render method from component object and return its innerHTML', () => {
+    const component = new TestComponent();
+    const innerHTML = render(component);
+
+    expect(innerHTML).toBe(component.element.innerHTML);
+  });
+
+  it('should behave normally if the component does not have a render method', () => {
+    const component = new EmptyComponent();
+    const innerHTML = render(component);
+
+    expect(innerHTML).toBe('');
+  });
+
+  it('should append to HTMLElement if specified', () => {
+    const element = document.createElement('div');
+    const component = new TestComponent();
+
+    render(component, element);
+
+    const isInside = within(component.element).queryByTestId('test-element');
+
+    expect(isInside).toBeTruthy();
   });
 });
