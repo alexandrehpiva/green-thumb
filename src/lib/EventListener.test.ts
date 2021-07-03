c88cc6fd6aec4e726ebb273fa7ec78430d87ab94
Alexandre Piva <alexandrehpiva@gmail.com>
2021-07-03 14:51:58 -0300

    Add EventListener tests

--- src/lib/EventListener.test.ts
+++ src/lib/EventListener.test.ts
@@ -1,13 +1,21 @@
 import EventListener from './EventListener';
 
-describe('Unit(EventListener) - subscribe method', () => {
-  it('should...', () => {
-    // TODO
-  });
-});
+describe('Unit(EventListener)', () => {
+  it('should be possible to subscribe to a event and then publish to it', () => {
+    const eventListener = new EventListener();
 
-describe('Unit(EventListener) - publish method', () => {
-  it('should...', () => {
-    // TODO
+    // Mock function
+    const fn = jest.fn((...payload) => payload);
+
+    // Subscription
+    eventListener.subscribe('change', fn);
+
+    // Publishing to change event with a payload
+    const testPayload = [123, 'test', { name: 'test' }];
+    const result = eventListener.publish('change', ...testPayload);
+
+    expect(fn).toBeCalledTimes(1);
+
+    expect(result).toEqual([testPayload]);
   });
 });
