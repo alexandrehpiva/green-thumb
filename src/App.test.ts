ad16e0c51def68e6443c4b49c28a2a0d1c5815d8
Alexandre Piva <alexandrehpiva@gmail.com>
2021-07-03 17:22:41 -0300

    Update App tests

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,12 +1,27 @@
-import App from "./App";
-import { queryById } from './utils/testUtils';
+import App from './App';
+import store from './store';
+import { cleanup, queryById } from './utils/testUtils';
 
 describe('App', () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
   it('should render the root div', async () => {
     App();
 
     const rootElement = queryById(document.body, 'root');
 
     expect(rootElement).toBeInTheDocument();
+  });
+
+  it('should publish a "rendered" event to store', () => {
+    const mockFn = jest.fn();
+
+    store.events.subscribe('rendered', mockFn);
+
+    App();
+
+    expect(mockFn).toBeCalledTimes(1);
   });
 });
